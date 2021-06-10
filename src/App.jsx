f5285fbc867298c779f319ddfd67e3f88ea46164
Alexandr Smirnov <alexsm.dev@gmail.com>
2021-06-10 16:45:03 +0300

    changed columns UserList

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import * as React from "react";
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import {UserList} from "./Users";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(json => console.log('all data', json))
-console.log('dataProvider', dataProvider)
+    .then(json => console.log('all users: ', json))
 
 const App = () => <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+    <Resource name="users" list={UserList} />
 </Admin>
 
 export default App;
